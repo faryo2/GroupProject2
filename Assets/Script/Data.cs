a4bf96ecb5a24d094ba79b283e31d0fc44702dd2
yuuuuuuuz <fff.yuzu.3156@gmail.com>
2023-07-03 20:47:43 +0900

    HighScore追加

--- Assets/Script/Data.cs
+++ Assets/Script/Data.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Data : MonoBehaviour
 {
   public float score;
+  
     // Start is called before the first frame update
     void Start()
     {
