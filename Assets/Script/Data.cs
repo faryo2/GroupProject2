9f7da4767a20df2c44e1d1dbcf418cc243b61492
avocado <ssp.10.01e@gmail.com>
2023-07-10 18:56:45 +0900

    Revert "Merge branch 'main' of https://github.com/faryo2/GroupProject2"
    This reverts commit 334bf7cbe339ab5781355396a82df36b4161b5eb, reversing
    changes made to 8dbe3687f180e93e02beda49b3cd42825fa9603c.

--- Assets/Script/Data.cs
+++ Assets/Script/Data.cs
@@ -1,12 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class Data : MonoBehaviour
 {
   public float score;
-  
     // Start is called before the first frame update
     void Start()
     {
